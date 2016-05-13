fa15f000c8f85ab7e529e0127b86f42f169dd7fe
Ryan Zimmerman <ryanwilliamzimmerman@gmail.com>
2016-05-12 22:54:43 -0400

    boilerplate

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+app.use(express.static('public'));
+
+
+
+
+
+app.listen(PORT, function(){
+	console.log('Express Server Started');
+});
